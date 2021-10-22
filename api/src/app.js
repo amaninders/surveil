8785e5b5818856d4dfa594c9ac8abcef9614a5ca
J Heslop <jayrese.h@gmail.com>
2021-10-22 15:13:59 -0400

    Use consistent quoting in api code

--- api/src/app.js
+++ api/src/app.js
@@ -9,7 +9,7 @@ const usersRouter = require("./routes/users");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.send('Index Router');
+router.get("/", (_, res) => {
+  res.send("Index Router");
 });
 
 module.exports = router;
