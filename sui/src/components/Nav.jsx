a6d10c76f08a3fd63f266e2cd0d85d52688a8aee
Harjeet Kaur <harjeet.kr1995@gmail.com>
2021-10-23 18:57:39 -0400

    EsLint fixes

--- sui/src/components/Nav.jsx
+++ sui/src/components/Nav.jsx
@@ -1,43 +1,46 @@
-import React from 'react'
-import NavItem from './NavItem';
+import React from "react";
+import NavItem from "./NavItem";
 
 function Nav(props) {
+  // sample navItems
+  const navItems = [
+    { name: "Home", feather: "home" },
+    { name: "Teams", feather: "file" },
+    { name: "Users", feather: "users" },
+  ];
 
-	// sample navItems
-	const navItems = [
-		{name:'Home', feather:'home'},
-		{name:'Teams', feather:'file'},
-		{name:'Users', feather:'users'}
-	]
+  // function to set view in the parent
+  const switchTo = (e, view) => {
+    e.preventDefault();
+    // console.log(view)
+    props.setView(view);
+  };
 
-	// function to set view in the parent
-	const switchTo = (e, view) => {
-		e.preventDefault();
-		// console.log(view)
-		props.setView(view)
-	}
+  // generate html for nav
+  const navHTML = navItems.map((item, index) => {
+    const selected = item.name === props.selected;
+    return (
+      <NavItem
+        key={index}
+        name={item.name}
+        feather={item.feather}
+        switchTo={switchTo}
+        ariaCurrent={selected}
+        class={selected ? "nav-item active" : "nav-item"}
+      />
+    );
+  });
 
-	// generate html for nav
-	const navHTML = navItems.map((item, index) => {
-		const selected = (item.name === props.selected)
-		return (<NavItem 
-			key={index} 
-			name={item.name}
-			feather={item.feather}
-			switchTo={switchTo}  
-			ariaCurrent={selected}
-			class={selected ? "nav-item active" : "nav-item"}
-		/>)})
-
-	return (
-	  <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-	    <div className="position-sticky pt-3">
-	      <ul className="nav flex-column">
-					{navHTML}
-	      </ul>
-	    </div>
-	  </nav>
-	)
+  return (
+    <nav
+      id="sidebarMenu"
+      className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
+    >
+      <div className="position-sticky pt-3">
+        <ul className="nav flex-column">{navHTML}</ul>
+      </div>
+    </nav>
+  );
 }
 
-export default Nav
+export default Nav;
