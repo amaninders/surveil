f442c39eadd7a96ae39622a3bb2c6b0550699143
Harjeet Kaur <harjeet.kr1995@gmail.com>
2021-10-23 18:56:17 -0400

    Add users component to display all users

--- sui/src/components/Main/Users.jsx
+++ sui/src/components/Main/Users.jsx
@@ -1,31 +1,128 @@
-import React from 'react'
-import UserItem from './UserItem'
+import { useState } from "react";
+import UsersTableData from "./Users/UsersTableData";
+import UsersTableHeader from "./Users/UsersTableHeader";
 
-function Users() {
-	const users = [
-		{name:'Aman', role: 'Student', status: 'Active'},
-		{name:'Aman', role: 'Student', status: 'Active'},
-		{name:'Aman', role: 'Student', status: 'Active'},
-		{name:'Aman', role: 'Student', status: 'Active'},
-		{name:'Aman', role: 'Student', status: 'Active'},
-		{name:'Aman', role: 'Student', status: 'Active'},
-		{name:'Aman', role: 'Student', status: 'Active'},
-		{name:'Aman', role: 'Student', status: 'Active'},
-		{name:'Aman', role: 'Student', status: 'Active'}
-	]
+import "./Users.css";
+import UsersHeader from "./Users/UsersHeader";
+import CreateUser from "./Users/CreateUser";
 
-	const userHTML = users.map((user, index) => 
-	<UserItem 
-		key={index}
-		{...user}
-	/>
-	)
+export default function Users() {
+  const usersArray = [
+    {
+      id: 1,
+      first_name: "Peter",
+      last_name: "Parkar",
+      team: "Sales",
+      manager: "Bob Smith",
+      progress_score: 60,
+    },
+    {
+      id: 2,
+      first_name: "John",
+      last_name: "Doe",
+      team: "Finance",
+      manager: "Mike Smith",
+      progress_score: 90,
+    },
+    {
+      id: 3,
+      first_name: "David",
+      last_name: "David",
+      team: "Sales",
+      manager: "James Jones",
+      progress_score: 40,
+    },
+    {
+      id: 4,
+      first_name: "Peter",
+      last_name: "Parkar",
+      team: "Sales",
+      manager: "Bob Smith",
+      progress_score: 60,
+    },
+    {
+      id: 5,
+      first_name: "John",
+      last_name: "Doe",
+      team: "Finance",
+      manager: "Mike Smith",
+      progress_score: 90,
+    },
+    {
+      id: 6,
+      first_name: "David",
+      last_name: "David",
+      team: "Sales",
+      manager: "James Jones",
+      progress_score: 40,
+    },
+    {
+      id: 7,
+      first_name: "Peter",
+      last_name: "Parkar",
+      team: "Sales",
+      manager: "Bob Smith",
+      progress_score: 60,
+    },
+    {
+      id: 8,
+      first_name: "John",
+      last_name: "Doe",
+      team: "Finance",
+      manager: "Mike Smith",
+      progress_score: 90,
+    },
+    {
+      id: 9,
+      first_name: "David",
+      last_name: "David",
+      team: "Sales",
+      manager: "James Jones",
+      progress_score: 40,
+    },
+    {
+      id: 10,
+      first_name: "Peter",
+      last_name: "Parkar",
+      team: "Sales",
+      manager: "Bob Smith",
+      progress_score: 30,
+    },
+    {
+      id: 11,
+      first_name: "John",
+      last_name: "Doe",
+      team: "Finance",
+      manager: "Mike Smith",
+      progress_score: 60,
+    },
+    {
+      id: 12,
+      first_name: "David",
+      last_name: "David",
+      team: "Sales",
+      manager: "James Jones",
+      progress_score: 80,
+    },
+  ];
 
-	return (
-		<ul>
-			{ userHTML}
-		</ul>
-	)
+  const [users, setUsers] = useState(usersArray);
+  const [newUser, setNewUser] = useState(false);
+
+  return (
+    <div className="users-container">
+      <UsersHeader setNewUser={setNewUser} />
+      {!newUser && (
+        <table className="table table-hover">
+          <tr>
+            <UsersTableHeader users={users} />
+          </tr>
+          <tbody>
+            <UsersTableData users={users} />
+          </tbody>
+        </table>
+      )}
+      {newUser && <CreateUser setNewUser={setNewUser} />}
+    </div>
+  );
 }
-
-export default Users
