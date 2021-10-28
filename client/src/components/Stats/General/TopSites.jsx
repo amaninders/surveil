007e84e189a5639865f3cd71911bfd9d953b103c
Harjeet Kaur <harjeet.kr1995@gmail.com>
2021-10-28 18:26:42 -0400

    Display top 5 sites

--- client/src/components/Stats/General/TopSites.jsx
+++ client/src/components/Stats/General/TopSites.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function TopSites() {
-	return (
-		<div class="col-md-8 data--item">
-			<h3>Top Applications/Sites</h3>
-			<div class="card">
-				<ul class="list-group list-group-flush">
-					<li class="list-group-item">An item</li>
-					<li class="list-group-item">An item</li>
-					<li class="list-group-item">An item</li>
-					<li class="list-group-item">An item</li>
-					<li class="list-group-item">An item</li>
-				</ul>
-			</div>
-		</div>
-	)
+  const [allSites, setAllSites] = useState([]);
+
+  useEffect(() => {
+    loadAllSites();
+  }, []);
+
+  //get all sites visited by users and time spent
+  const loadAllSites = async () => {
+    const allSites = await axios.get("http://localhost:8000/api/allsites", {
+      withCredentials: true,
+    });
+    setAllSites(allSites.data.slice(0,6));
+  };
+  return (
+    <div class="col-md-8 data--item">
+      <h3>Top Applications/Sites</h3>
+      <div class="card">
+        <ul class="list-group list-group-flush">
+					{allSites.map((site) =>(
+						<li class="list-group-item">{site.name}</li>
+					))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default TopSites
+export default TopSites;
