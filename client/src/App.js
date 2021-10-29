a13d4cbbf36a96e04aa3cb9249892d9d71b5e557
Harjeet Kaur <harjeet.kr1995@gmail.com>
2021-10-29 15:44:30 -0400

    Always show the stats for user_id 1 and team_id 1 by default

--- client/src/App.js
+++ client/src/App.js
@@ -9,60 +9,56 @@
 /
 */
 
-
-import './App.css';
-import Header from './components/Header';
-import Navigator from './components/Navigator';
-import Main from './components/Main/Index';
-import Teams from './components/Teams/Index';
-import Users from './components/Users/Index';
+import "./App.css";
+import Header from "./components/Header";
+import Navigator from "./components/Navigator";
+import Main from "./components/Main/Index";
+import Teams from "./components/Teams/Index";
+import Users from "./components/Users/Index";
 // import Landing from './components/Landing';
 
-import { BrowserRouter as Router,  Switch,  Route } from "react-router-dom";
-import { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
-
-	const [view, setView] = useState();
-	const [Id, setId] = useState({
-    id: null,
+  const [view, setView] = useState();
+  const [Id, setId] = useState({
+    id: 1,
   });
 
-	const menu = [
-		{	name:'home', to: '/main'},
-		{	name:'teams', to: '/teams'},
-		{ name:'users', to: '/users'}
-	]
+  const menu = [
+    { name: "home", to: "/main" },
+    { name: "teams", to: "/teams" },
+    { name: "users", to: "/users" },
+  ];
 
   return (
-		<Router>
-
-	    <div className="App">
-				<Header />
-				{/* <Landing /> */}
-				<main className="container py-5">
-					<Navigator menu={menu} />
-					<hr className="my-5" />
-					{/* A <Switch> looks through its children <Route>s and
+    <Router>
+      <div className="App">
+        <Header />
+        {/* <Landing /> */}
+        <main className="container py-5">
+          <Navigator menu={menu} />
+          <hr className="my-5" />
+          {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-	        <Switch>
-	          <Route path="/Teams">
-	            <Teams view={view} setView={setView} Id={Id} setId={setId}/>
-	          </Route>
-	          <Route path="/users">
-	            <Users view={view} setView={setView} Id={Id} setId={setId}/>
-	          </Route>
-	          <Route path="/main">
-	            <Main />
-	          </Route>
-						<Route path="/">
-	            <Main />
-	          </Route>
-	        </Switch>
-				</main>
-	    </div>
-
-		</Router>
+          <Switch>
+            <Route path="/Teams">
+              <Teams view={view} setView={setView} Id={Id} setId={setId} />
+            </Route>
+            <Route path="/users">
+              <Users view={view} setView={setView} Id={Id} setId={setId} />
+            </Route>
+            <Route path="/main">
+              <Main />
+            </Route>
+            <Route path="/">
+              <Main />
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 }
 
