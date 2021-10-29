ca03333bc9ac74f9fea88bad4a3c81d2d390c1cc
Harjeet Kaur <harjeet.kr1995@gmail.com>
2021-10-29 16:43:52 -0400

    Display activities for each user

--- client/src/components/Timeline/Index.jsx
+++ client/src/components/Timeline/Index.jsx
@@ -1,35 +1,37 @@
-import './Timeline.css'
-import React from 'react'
-import TimelineItem from './TimelineItem'
+import "./Timeline.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import TimelineItem from "./TimelineItem";
 
-function Timeline() {
-	return (
-		<div className="container">
-    <div className="row data--item">
+function Timeline(props) {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    loadActivities();
+  }, [props.Id]);
+
+  //get all Activities for a user
+  const loadActivities = async () => {
+    const allActivities = await axios.get(`http://localhost:8000/api/users/${props.Id.id}/activity`, {
+      withCredentials: true,
+    });
+    setActivities(allActivities.data);
+  };
+
+  return (
+    <div className="container">
+      <div className="row data--item">
         <div className="col-md-10">
-            <ul className="timeline">
-              <TimelineItem time={"12:13 pm"} appImage="favicon.ico" name={"facebook"} title={"lorem ipsum"} />
-              <TimelineItem time={"12:13 pm"} appImage="favicon.ico" name={"facebook"} title={"lorem ipsum"} />
-              <TimelineItem time={"12:13 pm"} appImage="favicon.ico" name={"facebook"} title={"lorem ipsum"} />
-              <TimelineItem time={"12:13 pm"} appImage="favicon.ico" name={"facebook"} title={"lorem ipsum"} />
-              <TimelineItem time={"12:13 pm"} appImage="favicon.ico" name={"facebook"} title={"lorem ipsum"} />
-              <TimelineItem time={"12:13 pm"} appImage="favicon.ico" name={"facebook"} title={"lorem ipsum"} />
-              <TimelineItem time={"12:13 pm"} appImage="favicon.ico" name={"facebook"} title={"lorem ipsum"} />
-              <TimelineItem time={"12:13 pm"} appImage="favicon.ico" name={"facebook"} title={"lorem ipsum"} />
-              <TimelineItem time={"12:13 pm"} appImage="favicon.ico" name={"facebook"} title={"lorem ipsum"} />
-              <TimelineItem time={"12:13 pm"} appImage="favicon.ico" name={"facebook"} title={"lorem ipsum"} />
-              <TimelineItem time={"12:13 pm"} appImage="favicon.ico" name={"facebook"} title={"lorem ipsum"} />
-              <TimelineItem time={"12:13 pm"} appImage="favicon.ico" name={"facebook"} title={"lorem ipsum"} />
-              <TimelineItem time={"12:13 pm"} appImage="favicon.ico" name={"facebook"} title={"lorem ipsum"} />
-              <TimelineItem time={"12:13 pm"} appImage="favicon.ico" name={"facebook"} title={"lorem ipsum"} />
-              <TimelineItem time={"12:13 pm"} appImage="favicon.ico" name={"facebook"} title={"lorem ipsum"} />
-              <TimelineItem time={"12:13 pm"} appImage="favicon.ico" name={"facebook"} title={"lorem ipsum"} />
-              <TimelineItem time={"12:13 pm"} appImage="favicon.ico" name={"facebook"} title={"lorem ipsum"} />
-            </ul>  
+          <ul className="timeline">
+            {activities.map(activity => <TimelineItem time={`${activity.start_time}`}
+              appImage="favicon.ico"
+              name={activity.name}
+              title={activity.title} />)}
+          </ul>
         </div>
+      </div>
     </div>
-</div>
-	)
+  );
 }
 
-export default Timeline
+export default Timeline;
