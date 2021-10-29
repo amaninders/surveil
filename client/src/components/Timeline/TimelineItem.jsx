bb6ecd1b53895c56c7113726f9d24faba2a6d217
Harjeet Kaur <harjeet.kr1995@gmail.com>
2021-10-29 18:54:48 -0400

    Add style to display date and time in different lines

--- client/src/components/Timeline/TimelineItem.jsx
+++ client/src/components/Timeline/TimelineItem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function TimelineItem(props) {
 	return (
 		<li>
-      <time className="time"><span>{props.time }</span> <span></span></time>
+      <time className="time"><span style={{whiteSpace: "pre"}}>{props.time }</span> <span></span></time>
       <div className="icon bg-icon"><i className="fas fa-chevron-circle-right"></i></div>
       <div className="label">
           <h2 className="align-self-center">Visited <span style={{color:'Green', fontSize:'1rem'}}>{props.name}</span> and app/page title was <span style={{color:'Green', fontSize:'1rem'}}>{props.title}</span></h2>
