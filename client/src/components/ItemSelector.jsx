1965305ae6e9c14aeb54b280e21fb04e43e4ee34
amaninders <amandeolofficial@gmail.com>
2021-10-29 03:18:01 -0400

    remove default select for date range on teams and users

--- client/src/components/ItemSelector.jsx
+++ client/src/components/ItemSelector.jsx
@@ -24,14 +24,14 @@ function ItemSelector(props) {
 			</div>
 			<div className="col-12">
 				<div className="btn-group" role="group" aria-label="Basic radio toggle button group" style={{width:'100%'}}>
-				  <input type="radio" className="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked />
-				  <label className="btn btn-outline-dark" for="btnradio1">Today</label>
+				  <input type="radio" className="btn-check" name="btnradio" id="btnradio1" autocomplete="off" />
+				  <label className="btn btn-outline-secondary" for="btnradio1">Today</label>
 
 				  <input type="radio" className="btn-check" name="btnradio" id="btnradio2" autocomplete="off" />
-				  <label className="btn btn-outline-dark" for="btnradio2">Week</label>
+				  <label className="btn btn-outline-secondary" for="btnradio2">Week</label>
 
 				  <input type="radio" className="btn-check" name="btnradio" id="btnradio3" autocomplete="off" />
-				  <label className="btn btn-outline-dark" for="btnradio3">Month</label>
+				  <label className="btn btn-outline-secondary" for="btnradio3">Month</label>
 				</div>
 			</div>
 		</div>
